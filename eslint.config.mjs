10fed43d5fe512534749a2f643a34c0bf79c1841
Teh Hippo <hippo@noemail.org>
2024-03-08 09:22:51 +1100

    Update linting

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default [
+  ...tseslint.config(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    eslint.configs.recommended,
+    ...tseslint.configs.stylisticTypeChecked,
+    ...tseslint.configs.strictTypeChecked,
+    {
+      languageOptions: {
+        parserOptions: {
+          project: true
+        }
+      }
+    }
+  ),
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: "single",
+    commaDangle: "never",
+    quoteProps: "as-needed",
+    arrowParens: false,
+    blockSpacing: true,
+    braceStyle: "1tbs",
+    flat: true,
+    semi: false
+  })
+];
